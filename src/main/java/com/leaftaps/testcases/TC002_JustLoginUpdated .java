31322eab28871cd14941c23c9e9a91eb50a419ce
sairampasunuri <sairam.pasunuri@gmail.com>
2023-09-02 15:40:05 +1000

    This is my first git hub udpate

---
+++ src/main/java/com/leaftaps/testcases/TC002_JustLoginUpdated .java
@@ -0,0 +1,28 @@
+package com.leaftaps.testcases;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.framework.testng.api.base.ProjectHooks;
+import com.leaftaps.pages.LoginPage;
+
+public class TC002_JustLoginUpdated extends ProjectHooks{
+	@BeforeTest
+	public void setValues() {
+		testcaseName = "Login";
+		testDescription ="Login to Leaftaps";
+		authors="Sairam";
+		category ="DevOps";
+		excelFileName="Login Second";
+	}
+	
+	@Test(dataProvider = "fetchData")
+	public void runLogin(String username, String password) {
+		new LoginPage()
+		.enterUsername(username)
+		.enterPassword(password)
+		.clickLogin();
+
+	}
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module GitDevopsEclips {
+	exports com.framework.selenium.api.design;
+	exports com.framework.selenium.api.base;
+	exports com.framework.testng.api.base;
+	exports com.leaftaps.pages;
+	exports com.leaftaps.testcases;
+	exports com.framework.utils;
+
+	requires java.datatransfer;
+	requires java.desktop;
+}
